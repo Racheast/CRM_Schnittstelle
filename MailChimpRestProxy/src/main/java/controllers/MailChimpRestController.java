1fecbb8e7147dcdb4747d57eb2d7917701228276
Alexander Eigner <alexander.eigner@gmm.local>
2017-12-05 15:31:17 +0100

    Implemented csv-file read for assigning cors mappings based on an external csv file on application startup.Removed system.out.printlns.removed some unneeded files.

--- MailChimpRestProxy/src/main/java/controllers/MailChimpRestController.java
+++ MailChimpRestProxy/src/main/java/controllers/MailChimpRestController.java
@@ -4,7 +4,6 @@ import java.security.NoSuchAlgorithmException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,28 +19,24 @@ import net.minidev.json.JSONObject;
 import service.RestService;
 
 import org.springframework.web.bind.annotation.RestController;
-
 import com.fasterxml.jackson.core.JsonProcessingException;
 
 @RestController
 public class MailChimpRestController {
 	@Autowired
-	RestService restService;
-		
-	@CrossOrigin(origins = "http://localhost:4848")
+	private RestService restService;
+	
+	//@CrossOrigin(origins = "http://localhost:4848")
 	@RequestMapping(value="/createSegment", method=RequestMethod.POST/*,produces = MediaType.APPLICATION_JSON_UTF8_VALUE*/)
 	public ResponseEntity<JSONObject> createSegment( @RequestParam String vApiPrefix, @RequestParam String baseURL, @RequestParam String vMCKey, @RequestParam String vMCList, @RequestBody SegmentDto segment) throws URISyntaxException {
 		Config config = new Config(baseURL,vMCKey,vApiPrefix,vMCList);
-		System.out.println("createSegment called!");
-		System.out.println("segment received: " + segment.toString());
 		return restService.createSegment(config,segment);
 	}
 	
-	@CrossOrigin(origins = "http://localhost:4848")
+	//@CrossOrigin(origins = "http://localhost:4848")
 	@RequestMapping(value="/addOrRemoveMembersFromSegment", method=RequestMethod.POST)
 	public ResponseEntity<JSONObject> addOrRemoveMembersFromSegment(@RequestParam String vApiPrefix, @RequestParam String baseURL, @RequestParam String vMCKey, @RequestParam String vMCList, @RequestParam String segment_id, @RequestBody MembersToAddAndRemoveDto membersToAddAndRemove) throws URISyntaxException{
 		Config config = new Config(baseURL,vMCKey,vApiPrefix,vMCList);
-		System.out.println("addOrRemoveMembersFromSegment() called!");
 
 		if(isInteger(segment_id)) {
 			return restService.addOrRemoveMembersFromSegment(config, membersToAddAndRemove.getMembers_to_add(), new String[0], Integer.parseInt(segment_id));
@@ -52,27 +47,24 @@ public class MailChimpRestController {
 		}
 	}
 	
-	@CrossOrigin(origins = "http://localhost:4848")
+	//@CrossOrigin(origins = "http://localhost:4848")
 	@RequestMapping(value="/addOrUpdateListMembers", method=RequestMethod.POST)
 	public ResponseEntity<JSONObject> addOrUpdateListMembers(@RequestParam String vApiPrefix, @RequestParam String baseURL, @RequestParam String vMCKey, @RequestParam String vMCList, @RequestBody MemberDto[] members) throws NoSuchAlgorithmException, JsonProcessingException, URISyntaxException{
 		Config config = new Config(baseURL,vMCKey,vApiPrefix,vMCList);
-		System.out.println("addOrUpdateListMembers called!");
 		return restService.addOrUpdateListMembers(config, members);
 	}
 	
-	@CrossOrigin(origins = "http://localhost:4848")
+	//@CrossOrigin(origins = "http://localhost:4848")
 	@RequestMapping(value="/getBatch", method=RequestMethod.GET)
 	public ResponseEntity<JSONObject> getBatch(@RequestParam String vApiPrefix, @RequestParam String baseURL, @RequestParam String vMCKey, @RequestParam String vMCList, @RequestParam String batch_id) throws NoSuchAlgorithmException, JsonProcessingException, URISyntaxException{
 		Config config = new Config(baseURL,vMCKey,vApiPrefix,vMCList);
-		System.out.println("getBatch called!");
 		return restService.getBatch(config, batch_id);
 	}
 	
-	@CrossOrigin(origins = "http://localhost:4848")
+	//@CrossOrigin(origins = "http://localhost:4848")
 	@RequestMapping(value="/getMemberFromList", method=RequestMethod.GET)
 	public ResponseEntity<JSONObject> getMemberFromList(@RequestParam String vApiPrefix, @RequestParam String baseURL, @RequestParam String vMCKey, @RequestParam String vMCList, @RequestParam String email_address) throws NoSuchAlgorithmException, JsonProcessingException, URISyntaxException{
 		Config config = new Config(baseURL,vMCKey,vApiPrefix,vMCList);
-		System.out.println("getMemberFromList called!");
 		return restService.getMemberFromList(config, email_address);
 	}
 	
