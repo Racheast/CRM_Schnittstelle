b995645a07beccb7e9f7a3dc83ea1e2519436643
Alexander Eigner <alexander.eigner@gmm.local>
2018-04-03 11:02:50 +0200

    Implemented log-file writing. Implemented manual selection of cors.csv path and port number on application start up, but commented it out due to export-to-jar issues (network project-paths maybe not supported?)

--- SAMSOAPProxy/src/main/java/SAMSOAPProxy/SamsoapProxyApplication.java
+++ SAMSOAPProxy/src/main/java/SAMSOAPProxy/SamsoapProxyApplication.java
@@ -12,8 +12,11 @@ import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -24,25 +27,41 @@ import com.opencsv.CSVReader;
 @SpringBootApplication(scanBasePackages = { "controller", "service", "util" })
 public class SamsoapProxyApplication {
 	private static Logger logger;
-
+	private static String corsPath;
+	private static int port;
+	
 	@Autowired
 	public static void main(String[] args) {
 		logger = Logger.getLogger(SamsoapProxyApplication.class);
 		logger.info("Starting SamSoapProxy ...");
-		ConfigurableApplicationContext context = SpringApplication.run(SamsoapProxyApplication.class, args);
-
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		try {
-			String command = br.readLine();
-			while (!command.equals("exit")) {
-				command = br.readLine();
+		if(args.length !=0) {  //hange to !=2
+			logger.error("Wrong number of parameters!");
+			logger.info("1. parameter: Path to cors.csv");
+			logger.info("2. parameter: Port on which this application should run.");
+		}else {
+			//corsPath = args[0];
+			//port = Integer.parseInt(args[1]);
+			//only for testing in eclipse run
+			corsPath = "." + File.separator + "src" + File.separator + "main" + File.separator
+							+ "resources" + File.separator + "corstest.csv";
+			//only for testing in eclipse run
+			port=8090;
+			
+			ConfigurableApplicationContext context = SpringApplication.run(SamsoapProxyApplication.class, new String[0]);
+	
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			try {
+				String command = br.readLine();
+				while (!command.equals("exit")) {
+					command = br.readLine();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
-		} catch (IOException e) {
-			e.printStackTrace();
+	
+			context.stop();
+			SpringApplication.exit(context);
 		}
-
-		context.stop();
-		SpringApplication.exit(context);
 	}
 
 	@Bean
@@ -51,9 +70,9 @@ public class SamsoapProxyApplication {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				try {
-					String filepath = "." + File.separator + "src" + File.separator + "main" + File.separator
-							+ "resources" + File.separator + "corstest.csv";
-					CSVReader reader = new CSVReader(new FileReader(filepath));
+					//String filepath = "." + File.separator + "src" + File.separator + "main" + File.separator
+					//		+ "resources" + File.separator + "corstest.csv";
+					CSVReader reader = new CSVReader(new FileReader(corsPath));  
 					List<String[]> lines = reader.readAll();
 					reader.close();
 					for (String[] line : lines) {
@@ -70,4 +89,13 @@ public class SamsoapProxyApplication {
 			}
 		};
 	}
+	
+	@Component
+	public class ContainerCustomizerBean implements EmbeddedServletContainerCustomizer {
+		
+		@Override
+		public void customize(ConfigurableEmbeddedServletContainer arg0) {
+			arg0.setPort(port);	
+		}
+	} 
 }
